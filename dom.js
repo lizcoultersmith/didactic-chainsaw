5adf2eadb42a1c30f3873e8592e4f71910bda07b
new name <liz@liz-macbook.local>
2019-01-20 08:17:39 +0000

    817

--- dom.js
+++ dom.js
@@ -1,13 +1,13 @@
 // EXAMINE THE DOCUMENT OBJECT //
 
-//console.dir(document);
+// console.dir(document);
 // console.log(document.domain);
 // console.log(document.URL);
 // console.log(document.title);
 // //document.title =  123;
 // console.log(document.doctype);
 // console.log(document.head);
-//console.log(document.body);
+// console.log(document.body);
 // console.log(document.all);
 // console.log(document.all[10]);
 // // document.all[10].textContent = 'Hello';
@@ -17,18 +17,17 @@
 
 // GETELEMENTBYID //
 // console.log(document.getElementById('header-title'));
-//var headerTitle = document.getElementById('header-title');
-//var header = document.getElementById('main-header');
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
 // console.log(headerTitle);
 // headerTitle.textContent = 'Hello';
 // headerTitle.innerText = 'Goodbye';
-//console.log(headerTitle.innerText);
-//headerTitle.innerHTML = '<h3>Hello</h3>';
-header.style.borderBottom = 'solid 3px #000';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+// header.style.borderBottom = 'solid 3px #000';
 
 // GETELEMENTSBYCLASSNAME //
-
-var items = document.getElementsByClassName('list-group-item');
+// var items = document.getElementsByClassName('list-group-item');
 // console.log(items);
 // console.log(items[1]);
 // items[1].textContent = 'Hello 2';
